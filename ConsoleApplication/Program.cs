4a1fc76ac5b8644ac59360742e423778250fdca1
Hunter Kaleb Perry <skittles2519@hotmail.com>
2020-01-07 00:44:34 -0700

    steps 1-4

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -6,7 +6,14 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var name = "Hunter";
+            var location = "Idaho Falls";
+            var date = DateTime.Now.Date.ToString("MM/dd/yyyy");
+            Console.WriteLine($"My name is {name}");
+            Console.WriteLine($"I am from {location}");
+            Console.WriteLine($"Todays date is {date} which means there are ");
+
+
         }
     }
 }
