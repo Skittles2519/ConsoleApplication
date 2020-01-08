c2571095b977978add3ced6ba163c498abce60cd
Hunter Kaleb Perry <skittles2519@hotmail.com>
2020-01-07 21:48:42 -0700

    Pretty much done

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -8,10 +8,41 @@ namespace ConsoleApplication
         {
             var name = "Hunter";
             var location = "Idaho Falls";
-            var date = DateTime.Now.Date.ToString("MM/dd/yyyy");
+            var now = DateTime.Now.Date;
+            DateTime christmas = new DateTime(2020,12,25);
+            var date = DateTime.Now.Date.ToString("MM/dd/yyyy"); 
+            double daysleft = christmas.Subtract(now).TotalDays;
             Console.WriteLine($"My name is {name}");
             Console.WriteLine($"I am from {location}");
-            Console.WriteLine($"Todays date is {date} which means there are ");
+            if (daysleft > 1)
+            {
+                Console.WriteLine($"Todays date is {date} which means there are {daysleft} days left until Christmas.");
+            }
+            else
+            {
+                Console.WriteLine($"Todays date is {date} which means there is 1 day left until Christmas.");
+            }
+            Console.WriteLine("Before you leave, enter a number");
+
+
+
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+            Console.WriteLine("Awesome, now 1 more");
+
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+            Console.WriteLine("The length of the wood is " +
+            woodLength + " feet");
+            Console.WriteLine("The area of the glass is " +
+            glassArea + " square meters");
+
+
+
 
 
         }
